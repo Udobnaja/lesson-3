0c9aa820aab430c6051f39a3046f400c13661436
udobnaja <udobnaja@gmail.com>
2018-03-15 02:17:32 +0300

    Инициализация проекта

---
+++ README.md
@@ -0,0 +1,9 @@
+# Мультимедиа
+
+## Команды
+
+Поставить зависимости `npm install` <br>
+Запустить проект `npm run start` <br>
+Собрать проект `npm run build` <br>
+Деплой проекта на github-pages `npm run deploy` <br>
+Линт `npm run lint`
---
+++ webpack.config.js
@@ -0,0 +1,108 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin"),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    CleanWebpackPlugin = require('clean-webpack-plugin'),
+    autoprefixer = require('autoprefixer');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: ['babel-loader']
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test:/\.(s*)css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback:'style-loader',
+                    use:[
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                minimize: true,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: [
+                                    autoprefixer({
+                                        browsers:['ie >= 8', 'last 4 version']
+                                    })
+                                ],
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                                sourceMapContents: true
+                            }
+                        }]
+                })
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                exclude: [/src\/images/],
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                exclude: [/src\/fonts/],
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images'
+                    }
+                }]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                exclude: [/src\/fonts/],
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            outputPath: 'images',
+                            optipng: {
+                                enabled: false
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html"
+        }),
+        new ExtractTextPlugin({
+            filename:'./[name].css'
+        })
+    ],
+    devtool : "source-map"
+};
